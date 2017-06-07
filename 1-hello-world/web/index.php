7ae6b8c548e1435cb941808e5c18d2a89583e61d
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2017-06-07 10:45:38 -0500

    Update index.php

--- 1-hello-world/web/index.php
+++ 1-hello-world/web/index.php
@@ -22,7 +22,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function () {
-    return 'Hello World';
+    return 'HOLA ALEXIS';
 });
 
 $app->get('/goodbye', function () {
